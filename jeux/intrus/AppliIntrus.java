a922629c3f9e69893785391bbb935f1ff9c071a3
Galiixy <50149050+Galiixy@users.noreply.github.com>
2019-05-04 23:26:07 +0200

    Update AppliIntrus.java

--- jeux/intrus/AppliIntrus.java
+++ jeux/intrus/AppliIntrus.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class AppliIntrus {
+public class AppliIntrus implements AppliJeu {
 	private static class Question {
 		public final List<String> mots;
 		public final int intrus;
@@ -18,22 +18,22 @@ public class AppliIntrus {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static String jouer() {
 		List<Question> questions = Arrays.asList(
 				new Question(Arrays.asList("pomme", "ananas", "poisson", "citron"), 3)
 				, new Question(Arrays.asList("fer", "igloo", "acier", "brique"), 2)
 				, new Question(Arrays.asList("eau", "lait", "jus", "fruit"), 4)
 				, new Question(Arrays.asList("chien", "table", "porte", "mur"), 1)
-				, new Question(Arrays.asList("chat", "girafe", "baleine", "histoire", "éléphant"), 4)
-				, new Question(Arrays.asList("stylo", "étiquette", "pinceau", "crayon"), 2)
-				, new Question(Arrays.asList("beau", "belle", "gentil", "lit", "agréable"), 4)
-				, new Question(Arrays.asList("dormir", "manger", "particulier", "rêver"), 3)
+				, new Question(Arrays.asList("chat", "girafe", "baleine", "histoire", "Ã©lÃ©phant"), 4)
+				, new Question(Arrays.asList("stylo", "Ã©tiquette", "pinceau", "crayon"), 2)
+				, new Question(Arrays.asList("beau", "belle", "gentil", "lit", "agrÃ©able"), 4)
+				, new Question(Arrays.asList("dormir", "manger", "particulier", "rÃªver"), 3)
 				, new Question(Arrays.asList("photo", "image", "poster", "carton"), 4)
 				, new Question(Arrays.asList("plume", "dune", "poil", "duvet"), 2)
 				);
 		Random r = new Random();
 		Question q = questions.get(r.nextInt(questions.size()));
-		System.out.println("Donnez le numéro de l'intrus parmi les mots qui suivent");
+		System.out.println("Donnez le numÃ©ro de l'intrus parmi les mots qui suivent");
 		int i = 0;
 		for (String m : q.mots)	
 			System.out.println((++i) + " : " + m);
@@ -47,7 +47,7 @@ public class AppliIntrus {
 				System.out.println("Dommage");
 		}
 		catch (Exception e) {
-			System.out.println("Raté");
+			System.out.println("RatÃ©");
 		}
 	}
 }
