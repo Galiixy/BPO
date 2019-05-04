bfa5c43cc0854b490c31d441dfa3c6536d2f4a26
Galiixy <50149050+Galiixy@users.noreply.github.com>
2019-05-05 00:37:08 +0200

    Add files via upload

--- jeux/devinettes/AppliDevinettes.java
+++ jeux/devinettes/AppliDevinettes.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 import AppliJeu.AppliJeu;
 
 public class AppliDevinettes implements AppliJeu{
-	public Etat jouer() {
+	public Etat jouer(String[] args) {
 		List<List<String>> devinettes = Arrays.asList( 
 				  Arrays.asList("Je suis entre 188 et 190, mais je ne suis pas 189. Qui suis-je ?", "et")
 				, Arrays.asList("J'ai une serrure mais pas de porte. Qui suis-je ?", "cadenas")
-				, Arrays.asList("Qu'est-ce qui t'appartient mais que les autres utilisent plus que toi ?", "prÃ©nom", "prenom")
-				, Arrays.asList("Je rentre toujours la premiÃ¨re et sors toujours la derniÃ¨re, qui suis-je ?", "clÃ©", "clef")
-				, Arrays.asList("ApprÃ©ciÃ©s en amour, les gros, eux, sont Ã  Ã©viter. Les auteurs en ont de fameux, et certains servent pour des jeux. Qui sont-ils ?", "mots")
-				, Arrays.asList("Lorsque l'on me nomme, je nâ€™existe plus. Qui suis-je ?", "silence")
+				, Arrays.asList("Qu'est-ce qui t'appartient mais que les autres utilisent plus que toi ?", "prénom", "prenom")
+				, Arrays.asList("Je rentre toujours la première et sors toujours la dernière, qui suis-je ?", "clé", "clef")
+				, Arrays.asList("Appréciés en amour, les gros, eux, sont à éviter. Les auteurs en ont de fameux, et certains servent pour des jeux. Qui sont-ils ?", "mots")
+				, Arrays.asList("Lorsque l'on me nomme, je n’existe plus. Qui suis-je ?", "silence")
 				, Arrays.asList("Combien de gouttes d'eau peut-on mettre dans un verre vide ?", "une")
 				);
 		Random r = new Random();
@@ -23,11 +23,11 @@ public class AppliDevinettes implements AppliJeu{
 		System.out.println(q.get(0));
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-		String rÃ©ponse = sc.nextLine().toLowerCase();
+		String réponse = sc.nextLine().toLowerCase();
 		for (int i = 1; i < q.size(); ++i)
-			if (rÃ©ponse.contains(q.get(i))) {
+			if (réponse.contains(q.get(i))) {
 				System.out.println("Bravo");
-				return Etat.gagnÃ©;
+				return Etat.gagné;
 			}
 		System.out.println("Dommage");
 		return Etat.perdu;
